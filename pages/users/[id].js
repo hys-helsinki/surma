e64d9adaa709f51d8cbd48abead35b0e53fcdb6f
Jesse Lankila <jesse.lankila@kodan.fi>
2022-07-10 23:56:47 +0300

    Use database-backed data for users list and user page

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,22 +1,31 @@
-import pelaajat from '../../pelaajat.json'
+import pelaajat from "../../pelaajat.json";
+import prisma from "../../lib/prisma";
 
 export async function getStaticProps({ params }) {
-  const userData = pelaajat.find(p => p.id === params.id)
+  const userData = await prisma.player.findUnique({
+    where: {
+      id: params.id,
+    },
+  });
   return {
     props: {
-      userData
+      ...userData,
+      description: {},
+      calendar: [],
     },
   };
 }
 
-export default function User({userData}) {
-  const cal = []
+export default function User(userData) {
+  const cal = [];
   for (const x in userData.calendar) {
-    cal.push([x, userData.calendar[x]])
+    cal.push([x, userData.calendar[x]]);
   }
   return (
     <div>
-      <h1>{userData.firstName} {userData.lastName}</h1>
+      <h1>
+        {userData.firstName} {userData.lastName}
+      </h1>
       <h2>alias: {userData.alias}</h2>
       <p>puhelinnumero: {userData.phone}</p>
       <p>email: {userData.email}</p>
@@ -30,24 +39,22 @@ export default function User({userData}) {
       <p>muu: {userData.description.other}</p>
       <h3>Kalenteri</h3>
       <ul>
-      {cal.map((c, index) =>
-        <li key={index}>
-          {c[0]}: {c[1]}
-        </li>
-      )}
+        {cal.map((c, index) => (
+          <li key={index}>
+            {c[0]}: {c[1]}
+          </li>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
 
-
 export async function getStaticPaths() {
-
-  const paths = pelaajat.map(user => ({
-    params : { id: user.id },
-  }))
+  const playerIds = await prisma.player.findMany({ select: { id: true } });
   return {
-    paths,
-    fallback: false
-  }
+    paths: playerIds.map((player) => ({
+      params: player,
+    })),
+    fallback: false,
+  };
 }
