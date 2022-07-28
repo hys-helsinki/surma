9f435b3027ce2def2c76ff2118e057833629b1e4
erikao1998 <ersku1998@gmail.com>
2022-07-28 12:51:50 +0300

    Form for updating the player data

---
+++ components/UpdateForm.tsx
@@ -0,0 +1,102 @@
+export const UpdateForm = ({ data }): JSX.Element => {
+  const cal = [];
+  for (const x in data.calendar) {
+    cal.push([x, data.calendar[x]]);
+  }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="address">
+            Kotiosoite:
+            <input
+              type="text"
+              id="address"
+              name="address"
+              defaultValue={data.address}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="learningInstitution">
+            Oppilaitos:
+            <input
+              type="text"
+              id="learningInstitution"
+              name="learningInstitution"
+              defaultValue={data.learningInstitution}
+            />
+          </label>
+        </div>
+        <h3>Kuvaus</h3>
+        <div>
+          <label htmlFor="eyeColor">
+            Silmät:
+            <input
+              type="text"
+              id="eyeColor"
+              name="eyeColor"
+              defaultValue={data.eyeColor}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="hair">
+            Hiukset:
+            <input type="text" id="hair" name="hair" defaultValue={data.hair} />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="height">
+            Pituus:
+            <input
+              type="text"
+              id="height"
+              name="height"
+              defaultValue={data.height}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="glasses">
+            Silmälasit:
+            <input
+              type="text"
+              id="glasses"
+              name="glasses"
+              defaultValue={data.glasses}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="other">
+            Muu:
+            <input
+              type="text"
+              id="other"
+              name="other"
+              defaultValue={data.other}
+            />
+          </label>
+        </div>
+        <div>
+          <h3>Kalenteri</h3>
+          {cal.sort().map((c, i) => (
+            <div key={i}>
+              <label htmlFor={c[0]}>
+                {c[0]}:
+                <textarea id={c[0]} name="dates" defaultValue={c[1]} />
+              </label>
+            </div>
+          ))}
+        </div>
+
+        <button type="submit">Tallenna muokkaukset</button>
+      </form>
+    </div>
+  );
+};
--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,39 +1,68 @@
-import { GetStaticProps, GetStaticPaths } from 'next';
-import { Player } from '@prisma/client'
-import { PlayerDetails } from '../../components/PlayerDetails';
-import { PlayerContactInfo } from '../../components/PlayerContactInfo';
-import prisma from '../../lib/prisma'
+import { GetStaticProps, GetStaticPaths } from "next";
+import { Player } from "@prisma/client";
+import { PlayerDetails } from "../../components/PlayerDetails";
+import { PlayerContactInfo } from "../../components/PlayerContactInfo";
+import prisma from "../../lib/prisma";
+import { MouseEventHandler } from "react";
+import { useState } from "react";
+import { UpdateForm } from "../../components/UpdateForm";
 
-export const getStaticProps: GetStaticProps = async ({params}) => {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const userData = await prisma.player.findUnique({
     where: {
-      id: params.id,
-    },
+      id: params.id
+    }
   });
   return {
-    props: userData,
+    props: userData
   };
-}
+};
 
-export default function User(
-  userData : Player
-  ): JSX.Element {
+export default function User(userData: Player): JSX.Element {
+  const [updateStatus, setUpdateStatus] = useState(true);
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = () => {
+    if (updateStatus === true) {
+      setUpdateStatus(false);
+    } else {
+      setUpdateStatus(true);
+    }
+  };
+
   return (
     <div>
-      <h1>{userData.firstName} {userData.lastName}</h1>
-      <PlayerContactInfo data={userData}/>
-      <PlayerDetails data={userData}/>
+      {updateStatus ? (
+        <div>
+          <h1>
+            {userData.firstName} {userData.lastName}
+          </h1>
+          <PlayerContactInfo data={userData} />
+          <PlayerDetails data={userData} />
+        </div>
+      ) : (
+        <div>
+          <h1>
+            {userData.firstName} {userData.lastName}
+          </h1>
+          <PlayerContactInfo data={userData} />
+          <UpdateForm data={userData} />
+        </div>
+      )}
+      <div>
+        <button onClick={handleClick}>
+          {updateStatus ? "muokkaa tietoja" : "peruuta"}
+        </button>
+      </div>
     </div>
-  )
+  );
 }
-
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const playerIds = await prisma.player.findMany({ select: { id: true } });
   return {
     paths: playerIds.map((player) => ({
-      params: player,
+      params: player
     })),
-    fallback: false,
+    fallback: false
   };
-}
+};
