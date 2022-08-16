37a00562a16768ae422e63cc7db6af2e928f5c97
erikao1998 <ersku1998@gmail.com>
2022-08-16 21:53:59 +0300

    date formatting

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -15,30 +15,49 @@ export const getStaticProps: GetStaticProps = async () => {
       umpires: true
     }
   });
-  tournaments = JSON.parse(JSON.stringify(tournaments)); // avoid Next.js serialization error
+  let newTournaments = [];
+  tournaments.map((tournament) =>
+    newTournaments.push({
+      ...tournament,
+      start: tournament.start.toString(),
+      end: tournament.end.toString(),
+      registrationStart: tournament.registrationStart.toString(),
+      registrationEnd: tournament.registrationEnd.toString()
+    })
+  );
+  newTournaments = JSON.parse(JSON.stringify(newTournaments));
   return {
-    props: { tournaments }
+    props: { newTournaments }
   };
 };
 
-export default function Admin({ tournaments }) {
+export default function Admin({ newTournaments }) {
   const router = useRouter();
   const switchPage = () => {
     router.push("/admin/create");
+  };
+
+  const modifyDate = (s) => {
+    console.log(s);
+    const date = new Date(s);
+    const formattedDate = `${date.getDate()}.${
+      date.getMonth() + 1
+    }.${date.getFullYear()} klo ${s.split(" ")[4]}`;
+    return formattedDate;
   };
   return (
     <div>
       <div>
         <button onClick={switchPage}>Lisää turnaus</button>
       </div>
-      {tournaments.map((tournament) => (
+      {newTournaments.map((tournament) => (
         <div key={tournament.id}>
           <p>{tournament.name}</p>
-          <p>Alkaa: {tournament.start}</p>
-          <p>Päättyy: {tournament.end}</p>
+          <p>Alkaa: {modifyDate(tournament.start)}</p>
+          <p>Päättyy: {modifyDate(tournament.end)}</p>
           <p>
-            Ilmoittautuminen: {tournament.registrationStart}-
-            {tournament.registrationEnd}
+            Ilmoittautuminen: {modifyDate(tournament.registrationStart)} -
+            {modifyDate(tournament.registrationEnd)}
           </p>
         </div>
       ))}
