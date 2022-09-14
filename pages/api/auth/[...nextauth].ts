81bd93390cee0e0e889b765c736f9f0b739b2275
Jesse Lankila <jesse.lankila@kodan.fi>
2022-09-15 01:12:40 +0300

    Email provider configured

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,22 @@
 import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/email";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "../../../lib/prisma";
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     // Passwordless / email sign in
     EmailProvider({
-      server: process.env.MAIL_SERVER,
-      from: "NextAuth.js <no-reply@example.com>"
+      server: {
+        host: process.env.SMTP_URL,
+        port: process.env.SMTP_PORT,
+        auth: {
+          user: process.env.SMTP_USERNAME,
+          pass: process.env.SMTP_PASSWORD
+        }
+      },
+      from: "Surma authorization <no-reply.surma@salamurhaajat.net>"
     })
   ]
 });
