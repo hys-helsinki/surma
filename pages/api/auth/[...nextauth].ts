7e341c8df2e3d917b68366859d58f55174d3f84b
Jesse Lankila <jesse.lankila@kodan.fi>
2022-09-18 22:57:01 +0300

    export auth config separately

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,7 +3,7 @@ import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "../../../lib/prisma";
 
-export default NextAuth({
+export const authConfig = {
   adapter: PrismaAdapter(prisma),
   providers: [
     // Passwordless / email sign in
@@ -19,4 +19,6 @@ export default NextAuth({
       from: "Surma authorization <no-reply.surma@salamurhaajat.net>"
     })
   ]
-});
+};
+
+export default NextAuth(authConfig);
