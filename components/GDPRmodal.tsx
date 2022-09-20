95c68f032bfad1d4494c559578f6b49bb42aa688
erikao1998 <59066641+erikao1998@users.noreply.github.com>
2022-09-20 13:34:48 +0300

    Finished registration form (#26)

---
+++ components/GDPRmodal.tsx
@@ -0,0 +1,47 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { useState } from "react";
+import GdprText from "./GDPRtext";
+
+const style = {
+  position: "absolute" as "absolute",
+  color: "black",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  height: "400px",
+  p: 4,
+  overflow: "scroll"
+};
+
+export default function GDPRModal() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <span>
+      <a onClick={handleOpen}>
+        <u>tietosuojaselosteen</u>
+      </a>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Tietosuojaseloste
+          </Typography>
+
+          <GdprText />
+        </Box>
+      </Modal>
+    </span>
+  );
+}
