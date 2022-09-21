4117c818d4c51c0daa8bd9b7f48334fd56217e3d
Jesse Lankila <jesse.lankila@kodan.fi>
2022-09-22 00:27:02 +0300

    Only show alias in ContactInfo if user has player

--- components/PlayerContactInfo.tsx
+++ components/PlayerContactInfo.tsx
@@ -1,7 +1,7 @@
 export const PlayerContactInfo = ({ user }): JSX.Element => {
   return (
     <div>
-      <h2>alias: {user.player.alias}</h2>
+      {user.player && <h2>alias: {user.player.alias}</h2>}
       <p>puhelinnumero: {user.phone}</p>
       <p>email: {user.email}</p>
     </div>
