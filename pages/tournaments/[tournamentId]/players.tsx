26adc0c81f961aecac82fa950689eb7b8f9952d5
Jesse Lankila <jesse.lankila@kodan.fi>
2022-09-29 00:13:43 +0300

    restrict access to player listing only for umpires

--- pages/tournaments/[tournamentId]/players.tsx
+++ pages/tournaments/[tournamentId]/players.tsx
@@ -2,8 +2,35 @@ import Link from "next/link";
 import { GetServerSideProps } from "next";
 import prisma from "../../../lib/prisma";
 import { AuthenticationRequired } from "../../../components/AuthenticationRequired";
+import { unstable_getServerSession } from "next-auth";
+import { authConfig } from "../../api/auth/[...nextauth]";
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+const isCurrentUserAuthorized = async (tournamentId, context) => {
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authConfig
+  );
+
+  const umpire = await prisma.umpire.findFirst({
+    where: {
+      userId: session.user.id,
+      tournamentId: tournamentId
+    }
+  });
+  console.log("access check for tournament", tournamentId);
+  console.log("checking current user umpire status:", umpire);
+  return umpire != null;
+};
+
+export const getServerSideProps: GetServerSideProps = async ({
+  params,
+  ...context
+}) => {
+  if (!(await isCurrentUserAuthorized(params.tournamentId, context))) {
+    console.log("is not authorized!");
+    return { redirect: { destination: "/personal", permanent: false } };
+  }
   const objects = await prisma.player.findMany({
     select: {
       id: true,
