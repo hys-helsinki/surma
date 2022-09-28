d0e299c63c0571f50dbdf018c8d966725727d875
Jesse Lankila <jesse.lankila@kodan.fi>
2022-09-28 10:18:19 +0300

    make tournament index an empty page with redirect

--- pages/tournaments/[tournamentId]/index.tsx
+++ pages/tournaments/[tournamentId]/index.tsx
@@ -1,1 +1,12 @@
-export {};
+export async function getServerSideProps(context) {
+  return {
+    redirect: {
+      destination: "/personal",
+      permanent: false
+    }
+  };
+}
+
+export default function Personal(): JSX.Element {
+  return <></>;
+}
