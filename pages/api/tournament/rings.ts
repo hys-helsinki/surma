437e01da46b1b399b4941f3072837578cd298708
Jesse Lankila <jesse.lankila@kodan.fi>
2022-09-28 23:10:22 +0300

    protect ring and user api routes

--- pages/api/tournament/rings.ts
+++ pages/api/tournament/rings.ts
@@ -1,9 +1,27 @@
 import prisma from "../../../lib/prisma";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { unstable_getServerSession } from "next-auth";
+import { authConfig } from "../auth/[...nextauth]";
+
+const isCurrentUserAuthorized = async (tournamentId, req, res) => {
+  const session = await unstable_getServerSession(req, res, authConfig);
+
+  const umpire = await prisma.umpire.findFirst({
+    where: {
+      userId: session.user.id,
+      tournamentId: tournamentId
+    }
+  });
+  return umpire;
+};
 
 export default async function rings(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     const newRing = JSON.parse(req.body);
+    // TODO make a dynamic route to supply tournament id as path parameter and move this before method check
+    if (!isCurrentUserAuthorized(newRing.tournament, req, res)) {
+      res.status(403).end();
+    }
     const savedRing = await prisma.assignmentRing.create({
       data: {
         name: newRing.name,
