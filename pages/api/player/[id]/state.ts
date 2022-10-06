ea455172c35338203a673d45f8513d7426cbe711
Jesse Lankila <jesse@kodan.fi>
2022-10-06 22:47:36 +0300

    Introduce player state and endpoint for state changes (#43)

---
+++ pages/api/player/[id]/state.ts
@@ -0,0 +1,43 @@
+import prisma from "../../../../lib/prisma";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { unstable_getServerSession } from "next-auth";
+import { authConfig } from "../../auth/[...nextauth]";
+
+const isCurrentUserAuthorized = async (playerId, req, res) => {
+  const session = await unstable_getServerSession(req, res, authConfig);
+
+  const updatedPlayer = await prisma.player.findFirst({
+    where: {
+      id: playerId,
+      userId: session.user.id
+    },
+    select: {
+      tournamentId: true
+    }
+  });
+  const umpire = await prisma.umpire.findFirst({
+    where: {
+      userId: session.user.id,
+      tournamentId: updatedPlayer.tournamentId
+    }
+  });
+  return umpire != null;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (!isCurrentUserAuthorized(req.query.id, req, res)) {
+    res.status(403).end();
+  }
+  if (req.method === "PATCH") {
+    const playerId = req.query.id as string;
+    const { state } = JSON.parse(req.body);
+    const result = await prisma.player.update({
+      where: { userId: playerId },
+      data: { state }
+    });
+    res.status(204).end();
+  }
+}
