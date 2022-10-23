186dc13ffea9b8439331ab1b223888af4f132525
Jesse Lankila <jesse.lankila@kodan.fi>
2022-10-23 20:57:33 +0300

    Fix /personal redirect

--- pages/personal.tsx
+++ pages/personal.tsx
@@ -1,5 +1,6 @@
 import { unstable_getServerSession } from "next-auth";
 import { authConfig } from "./api/auth/[...nextauth]";
+import prisma from "../lib/prisma";
 import { AuthenticationRequired } from "../components/AuthenticationRequired";
 
 export async function getServerSideProps(context) {
@@ -8,9 +9,17 @@ export async function getServerSideProps(context) {
     context.res,
     authConfig
   );
+  const currentUser = await prisma.user.findUnique({
+    select: {
+      tournamentId: true
+    },
+    where: {
+      id: session.user.id
+    }
+  });
   return {
     redirect: {
-      destination: "/tournaments/users/".concat(session.user.id),
+      destination: `/tournaments/${currentUser.tournamentId}/users/${session.user.id}`,
       permanent: false
     }
   };
