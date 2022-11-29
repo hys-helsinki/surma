a2ccc40aff046cfd6c4d73232776a7f53055153e
erikao1998 <ersku1998@gmail.com>
2022-11-29 23:05:47 +0200

    redirect user to the user page if they are logged in

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,19 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export default function Home() {
   const { data: session } = useSession();
   if (session) {
-    return (
-      <div className={styles.container}>
-        <h1>Surma (Murhamaster 3.0)</h1>
-        <button onClick={() => signOut()}>Kirjaudu ulos</button>
-      </div>
-    );
+    const router = useRouter();
+    router.push("/personal");
+    // return (
+    //   <div className={styles.container}>
+    //     <h1>Surma (Murhamaster 3.0)</h1>
+    //     <button onClick={() => signOut()}>Kirjaudu ulos</button>
+    //   </div>
+    // );
   }
   return (
     <>
