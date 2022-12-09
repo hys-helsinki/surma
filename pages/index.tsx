79ba4ad7f659c201660429fbb5b5ae2705b40000
erikao1998 <ersku1998@gmail.com>
2022-12-09 16:14:38 +0200

    added a loading screen to index page while it checks if the user has session

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,26 +3,46 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { TailSpin } from "react-loader-spinner";
 
 export default function Home() {
+  const [sessionChecker, setSessionChecker] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setSessionChecker(false), 5000);
+  }, []);
+
   const { data: session } = useSession();
   if (session) {
     const router = useRouter();
     router.push("/personal");
-    // return (
-    //   <div className={styles.container}>
-    //     <h1>Surma (Murhamaster 3.0)</h1>
-    //     <button onClick={() => signOut()}>Kirjaudu ulos</button>
-    //   </div>
-    // );
   }
   return (
     <>
-      Et ole kirjautunut sisään.
-      <br />
-      <button onClick={() => signIn("email", { callbackUrl: "/personal" })}>
-        Kirjaudu sisään
-      </button>
+      {sessionChecker ? (
+        <div style={{ margin: "10px" }}>
+          <h3>Tarkistetaan onko käyttäjä kirjautunut ...</h3>
+          <TailSpin
+            height="60"
+            width="60"
+            color="#fafafa"
+            ariaLabel="tail-spin-loading"
+            radius="1"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        </div>
+      ) : (
+        <div style={{ margin: "10px" }}>
+          Et ole kirjautunut sisään.
+          <br />
+          <button onClick={() => signIn("email", { callbackUrl: "/personal" })}>
+            Kirjaudu sisään
+          </button>
+        </div>
+      )}
     </>
   );
 }
