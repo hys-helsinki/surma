2526e0c69bb6ac25b027a9bd8f9c9e439163e200
Erika <ersku1998@gmail.com>
2024-06-23 22:01:51 +0300

    Update create user API endpoint

--- pages/api/user/create.ts
+++ pages/api/user/create.ts
@@ -1,6 +1,5 @@
 import prisma from "../../../lib/prisma";
 import type { NextApiRequest, NextApiResponse } from "next";
-import sendEmail from "../../../lib/ses_mailer";
 
 export default async function create(
   req: NextApiRequest,
@@ -12,31 +11,10 @@ export default async function create(
       data: {
         firstName: playerData.firstName,
         lastName: playerData.lastName,
-        phone: playerData.phone,
         email: playerData.email,
         tournament: { connect: { id: playerData.tournamentId } },
-        player: {
-          create: {
-            tournament: { connect: { id: playerData.tournamentId } },
-            alias: playerData.alias,
-            address: playerData.address,
-            learningInstitution: playerData.learningInstitution,
-            eyeColor: playerData.eyeColor,
-            hair: playerData.hair,
-            height: playerData.height,
-            glasses: playerData.glasses,
-            other: playerData.other,
-            calendar: playerData.calendar
-          }
-        }
       }
     });
-    sendEmail(
-      "tuomaristo@salamurhaajat.net",
-      playerData.email,
-      "Kiitos ilmoittautumisesta!",
-      "Kiitos ilmoittautumisestasi! Tuomaristo tarkastaa sen lähiaikoina."
-    );
-    res.status(201).json(result);
+    res.status(201).send(result);
   }
 }
