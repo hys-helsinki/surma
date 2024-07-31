f99667034ba617811b162ac90def1a981ad5ab9a
Sara Pyykölä <sara.pyykola@gmail.com>
2024-07-31 17:12:17 +0300

    css refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,33 +48,33 @@ export default function Home({ tournaments }) {
   return (
   <div>
       <div>
-        <button onClick={() => signIn("email", { callbackUrl: "/personal" })} style={{ float: "right", display: "inline"}}>Kirjaudu sisään</button>
+        <button onClick={() => signIn("email", { callbackUrl: "/personal" })} style={{ float: "right", display: "inline", margin: "auto", marginBottom: "30px"}}>Kirjaudu sisään</button>
       </div>
     <div className={styles.center}>
-      <h1 style={{ textAlign: "center", justifyContent: "center", display: "inline"}}>Surma (Murhamaster 3.0)</h1>
-    <div className={styles.main}>
+      <h1>Surma (Murhamaster 3.0)</h1>
+    <div>
       <Image src={logo} alt="logo" width={200} height={200} />
       <h2> Avoimet salamurhaturnaukset </h2>
       <p> Alla on lista tulevista turnauksista. Jos alla ei näy mitään, tulevia turnauksia ei ole juuri nyt tiedossa, joten palaathan myöhemmin takaisin. Tietoa tulevista turnauksista voi saada myös <a href="https://salamurhaajat.net/tulevat-tapahtumat">HYSin nettisivuilta.</a></p>
       {tournaments.map((tournament) => (
         <div key={tournament.id}>
           <Link href={`/registration/${tournament.id}`}>
-          <table aria-label="tournament-table" style={{borderSpacing: "15px", marginBottom: "30px"}}>
+          <table aria-label="tournament-table" className={styles.tournament_table}>
           <tr>
-            <th style={{width:"15%", padding: "15px"}}>Nimi</th>
-            <th style={{width:"10%", padding: "15px"}}>Aika</th>
-            <th style={{width:"10%", padding: "15px"}}>Ilmoittautuminen käynnissä</th>
-            <th style={{width:"15%", padding: "15px"}}></th>
+            <th className={styles.tournament_th}>Nimi</th>
+            <th className={styles.tournament_th}>Turnaus käynnissä</th>
+            <th className={styles.tournament_th}>Ilmoittautuminen käynnissä</th>
+            <th className={styles.tournament_th}></th>
           </tr>
           <tr>
-            <td>{tournament.name}</td>
-            <td>
+            <td className={styles.tournament_td}>{tournament.name}</td>
+            <td className={styles.tournament_td}>
             {modifyDate(tournament.startTime)}&nbsp;-&nbsp;{modifyDate(tournament.endTime)}
             </td>
-            <td>
+            <td className={styles.tournament_td}>
             {modifyDate(tournament.registrationStartTime)}&nbsp;-&nbsp;{modifyDate(tournament.registrationEndTime)}
             </td>
-            <td><a>Ilmoittautumislomake</a></td>
+            <td className={styles.tournament_td}><a>Ilmoittautumislomake</a></td>
           </tr>
         </table>
           </Link>
@@ -94,10 +94,11 @@ export default function Home({ tournaments }) {
         >
           <h2 className={styles.center}>Mikä on Surma?</h2>
         </AccordionSummary>
-        <AccordionDetails sx={{
-          backgroundColor: "rgb(34, 23, 23)",
-          color: "white"}}>
-          Surma on leikkisten salamurhaturnausten järjestämiseen tarkoitettu ohjelma, jota ylläpitää ja kehittää Helsingin yliopiston salamurhapelaajat ry:n aktiivit eli <a href="https://github.com/hys-helsinki">GitHubin hys-helsinki -organisaatio.</a> Surmaa käytetään salamurhaturnauksien ylläpitämiseen: pelaajat jakavat tietoja toisilleen Surman kautta turnauksen ajan.
+        <AccordionDetails 
+        sx={{
+        backgroundColor: "rgb(34, 23, 23)",
+        color: "white"}}>
+        Surma on leikkisten salamurhaturnausten järjestämiseen tarkoitettu ohjelma, jota ylläpitää ja kehittää Helsingin yliopiston salamurhapelaajat ry:n aktiivit eli <a href="https://github.com/hys-helsinki">GitHubin hys-helsinki -organisaatio.</a> Surmaa käytetään salamurhaturnauksien ylläpitämiseen: pelaajat jakavat tietoja toisilleen Surman kautta turnauksen ajan.
         </AccordionDetails>
         </Accordion>
 
@@ -113,16 +114,17 @@ export default function Home({ tournaments }) {
         >
           <h2 className={styles.center}>Mikä on salamurhaturnaus?</h2>
         </AccordionSummary>
-        <AccordionDetails sx={{
-          backgroundColor: "rgb(34, 23, 23)",
-          color: "white"}}>
-          <p>Salamurhaturnauksessa pelaajat elävät arkista elämäänsä samalla kun he pyrkivät hankkiutumaan muiden ihmisten huomaamatta olosuhteisiin, joissa voivat soveltaa toisiin pelaajiin sääntöjen sallimaa kirjoa leikkimielisiä aseenkorvikkeita. 
-          Turnausta pelataan osallistujien kesken murharingeittäin: pelaaja A:n tavoite on salamurhata kohde eli pelaaja B, pelaaja B saalistaa pelaaja C:tä ja niin edelleen aina pelaajaan N asti, joka väijyy pelaajaa A. 
-          Mikäli A onnistuu murhaamaan B:n sääntöjen sallimalla tavalla, A ja B ottavat yhteyttä tuomaristoon ja raportoivat tapahtumien kulusta pisteytystä varten. Nyt B putoaa pelistä, jolloin A alkaakin väijyä C:tä uutena kohteenaan. 
-          Peliä pelataan näin kellon ympäri ennalta määrättyyn aikarajaan asti, jolloin pelin voittaja päätetään pisteillä. Pelin helpottamiseksi pelaajat jakavat muun muuassa kalentereitaan ja kotiosoitteitaan toisilleen Surmassa.</p>
-          <p>Salamurhaturnaukseen osallistuminen aloitetaan ilmoittautumalla peliin Surman kautta, jonka jälkeen Surma lähettää lisäohjeita sähköpostilla. </p>
-          <p>Lue lisää pelaajatiedoista ja niiden käsittelystä Surman tietosuojaselosteesta.</p> 
-          <p>Lisätietoja salamurhaturnauksesta ja sen säännöistä saa taas <a href="https://salamurhaajat.net/mika-salamurhapeli">Helsingin yliopiston salamurhapelaajien alias HYSin nettisivuilta.</a></p>
+        <AccordionDetails 
+        sx={{
+        backgroundColor: "rgb(34, 23, 23)",
+        color: "white"}}>
+        <p>Salamurhaturnauksessa pelaajat elävät arkista elämäänsä samalla kun he pyrkivät hankkiutumaan muiden ihmisten huomaamatta olosuhteisiin, joissa voivat soveltaa toisiin pelaajiin sääntöjen sallimaa kirjoa leikkimielisiä aseenkorvikkeita. 
+        Turnausta pelataan osallistujien kesken murharingeittäin: pelaaja A:n tavoite on salamurhata kohde eli pelaaja B, pelaaja B saalistaa pelaaja C:tä ja niin edelleen aina pelaajaan N asti, joka väijyy pelaajaa A. 
+        Mikäli A onnistuu murhaamaan B:n sääntöjen sallimalla tavalla, A ja B ottavat yhteyttä tuomaristoon ja raportoivat tapahtumien kulusta pisteytystä varten. Nyt B putoaa pelistä, jolloin A alkaakin väijyä C:tä uutena kohteenaan. 
+        Peliä pelataan näin kellon ympäri ennalta määrättyyn aikarajaan asti, jolloin pelin voittaja päätetään pisteillä. Pelin helpottamiseksi pelaajat jakavat muun muuassa kalentereitaan ja kotiosoitteitaan toisilleen Surmassa.</p>
+        <p>Salamurhaturnaukseen osallistuminen aloitetaan ilmoittautumalla peliin Surman kautta, jonka jälkeen Surma lähettää lisäohjeita sähköpostilla. </p>
+        <p>Lue lisää pelaajatiedoista ja niiden käsittelystä Surman tietosuojaselosteesta.</p> 
+        <p>Lisätietoja salamurhaturnauksesta ja sen säännöistä saa taas <a href="https://salamurhaajat.net/mika-salamurhapeli">Helsingin yliopiston salamurhapelaajien alias HYSin nettisivuilta.</a></p>
         </AccordionDetails>
       </Accordion>
       </div>
