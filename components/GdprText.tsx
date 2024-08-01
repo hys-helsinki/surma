81c786b7a532e84ead593aede8b3a76d6c635fca
Sara Pyykölä <sara.pyykola@gmail.com>
2024-08-01 17:28:04 +0300

    updated modal text

--- components/GdprText.tsx
+++ components/GdprText.tsx
@@ -5,7 +5,7 @@ export default function GdprText() {
       <ul>
         <li>Me vain säilytämme tietojasi, emme tee analytiikkaa.</li>
         <li>Käytämme tietojasi vain Surman ylläpitoon ja salamurhaturnauksen järjestämiseen. Tuhoamme tietosi Surmasta turnauksen jälkeen.</li>
-        <li>Annamme luotettaville palveluntarjoajillemme tietoja vain sen verran, mikä on pakko palveluiden saamiseksi.</li>
+        <li>Annamme luotettaville palveluntarjoajillemme tietoja vain sen verran, mikä on pakko palveluiden saamiseksi, ja annamme pelaajille tietoja vain sen verran, mitä tarvitaan turnaukseen osallistumiseen.</li>
       </ul>
     </div>
   );
