6493bdad8448040509fbd11c72ce892fd331d2e7
Erika <ersku1998@gmail.com>
2024-08-09 21:09:23 +0300

    Render calendar correctly in the player page

--- components/PlayerPage/Calendar.tsx
+++ components/PlayerPage/Calendar.tsx
@@ -1,40 +1,23 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
+import { getTournamentDates, splitCalendar } from "../utils";
 
 export const Calendar = ({ player, tournament }): JSX.Element => {
   const [weekNumber, setSlideNumber] = useState(0);
   const [isUpdated, setIsUpdated] = useState(true);
   const router = useRouter();
 
-  const { id } = router.query;
+  const { id: userId } = router.query;
 
-  const start = new Date(tournament.startTime);
-  const end = new Date(tournament.endTime);
+  const dates = getTournamentDates(
+    new Date(tournament.startTime),
+    new Date(tournament.endTime)
+  );
 
-  const dates: Array<any> = [];
-
-  dates.push(`${start.getDate()}.${start.getMonth() + 1}.`);
-  let loopDay = start;
-  while (loopDay < end) {
-    loopDay.setDate(loopDay.getDate() + 1);
-    dates.push(`${loopDay.getDate()}.${loopDay.getMonth() + 1}.`);
-  }
-
-  const cal = [];
-  for (const x in player.calendar) {
-    cal.push([x, player.calendar[x]]);
-  }
-
-  let chunks = [];
-  const chunkSize = 7;
-
-  for (let i = 0; i < cal.length; i += chunkSize) {
-    const chunk = cal.slice(i, i + chunkSize);
-    chunks.push(chunk);
-  }
+  const weeks = splitCalendar(player.calendar);
 
   const handleSlideShow = (event) => {
-    if (weekNumber == chunks.length - 1) {
+    if (weekNumber == weeks.length - 1) {
       setSlideNumber(0);
     } else {
       setSlideNumber(weekNumber + 1);
@@ -44,16 +27,6 @@ export const Calendar = ({ player, tournament }): JSX.Element => {
   const handleCalendarSubmit = async (
     event: React.FormEvent<HTMLFormElement>
   ) => {
-    const start = new Date(tournament.startTime);
-    const end = new Date(tournament.endTime);
-    let dates: Array<any> = [];
-    dates.push(`${start.getDate()}.${start.getMonth() + 1}.`);
-    let loopDay = start;
-    while (loopDay < end) {
-      loopDay.setDate(loopDay.getDate() + 1);
-      dates.push(`${loopDay.getDate()}.${loopDay.getMonth() + 1}.`);
-    }
-
     const cal = {};
     dates.forEach((x, i) => (cal[x] = event.currentTarget.dates[i].value));
     event.preventDefault();
@@ -62,7 +35,7 @@ export const Calendar = ({ player, tournament }): JSX.Element => {
     };
 
     try {
-      await fetch(`/api/user/update/${id}`, {
+      await fetch(`/api/user/update/${userId}`, {
         method: "PUT",
         body: JSON.stringify(data)
       });
@@ -74,22 +47,22 @@ export const Calendar = ({ player, tournament }): JSX.Element => {
 
   return (
     <div className="calendar">
-      <h3 style={{ width: "40%", margin: "auto", padding: "10px" }}>
-        Kalenteri
-      </h3>
       <button onClick={() => setIsUpdated(!isUpdated)}>
         {isUpdated ? "Muokkaa kalenteria" : "Peruuta"}
       </button>
+      <h3 style={{ width: "40%", margin: "auto", padding: "10px" }}>
+        Kalenteri
+      </h3>
 
       {isUpdated ? (
         <div>
           <ul>
-            {chunks[weekNumber].map((c, index) => (
+            {weeks[weekNumber].map((calendarElement: string[], index) => (
               <li
                 key={index}
                 style={{ paddingBottom: "20px", whiteSpace: "pre-line" }}
               >
-                {c[0]}: {c[1]}
+                {calendarElement[0]}: {calendarElement[1]}
               </li>
             ))}
           </ul>
@@ -99,7 +72,7 @@ export const Calendar = ({ player, tournament }): JSX.Element => {
         </div>
       ) : (
         <div>
-          <form onSubmit={handleCalendarSubmit}>
+          {/* <form onSubmit={handleCalendarSubmit}>
             <button type="submit">Tallenna muokkaukset</button>
             {cal.map((c, i) => (
               <div key={i}>
@@ -109,7 +82,8 @@ export const Calendar = ({ player, tournament }): JSX.Element => {
                 </label>
               </div>
             ))}
-          </form>
+          </form> */}
+          lol
         </div>
       )}
     </div>
