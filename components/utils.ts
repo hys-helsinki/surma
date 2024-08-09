c1150559320522e2bc086e81d2cf0d8e3067b150
Erika <ersku1998@gmail.com>
2024-08-09 21:08:37 +0300

    Create utils function for splitting calendar into week chunks + add year to the calendar date string

--- components/utils.ts
+++ components/utils.ts
@@ -1,7 +1,21 @@
 export const getTournamentDates = (start: Date, end: Date) => {
   const dates = [];
   for (const date = start; date <= end; date.setDate(date.getDate() + 1)) {
-    dates.push(`${date.getDate()}.${date.getMonth() + 1}`);
+    dates.push(
+      `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`
+    );
   }
   return dates;
+};
+
+export const splitCalendar = (calendar: string[][]) => {
+  const weeks: string[][][] = [];
+  const chunkSize = 7;
+
+  for (let i = 0; i < calendar.length; i += chunkSize) {
+    const chunk = calendar.slice(i, i + chunkSize);
+    weeks.push(chunk);
+  }
+
+  return weeks;
 };
