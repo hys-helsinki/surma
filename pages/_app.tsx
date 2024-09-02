3a6889be04a5c8696b11be843606a25099521759
Erika Ojanperä <59066641+erikao1998@users.noreply.github.com>
2024-09-02 22:32:39 +0300

    Update favicon (#105)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
+import Head from "next/head";
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider>
+      <Head>
+        <title>Surma</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
