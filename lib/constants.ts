6a3d39411d7ad1b93547916f074dd2a4d030745d
Erika Ojanperä <59066641+erikao1998@users.noreply.github.com>
2024-09-10 16:02:14 +0300

    Kalenterimuutoksia (#101)
    * Wrap the player page stuff inside a container to add margins to the sides
    
    * Add responsibility to the umpire model
    
    * Add theme to tournament
    
    * Refactor player page
    
    * Move components to a correct folder
    
    * More refactoring
    
    * Update how umpires are shown in the player page
    
    * Show player state
    
    * More player page refactoring because why not
    
    * Render tabs in the player page when using smaller devices
    
    * Some tab fixes
    
    * More refactoring to make the mobile view work nicely
    
    * Update umpire listing in the player page
    
    * Show security data in the player page
    
    * Use formik also in the details update form
    
    * Remove useless stuff from the DesktopView component
    
    * Comment one line to fix build (the file will be updated later)
    
    * Add button to update player image
    
    * Small style fix
    
    * Fix player data updating
    
    * Add admin page and tournament rules links to navbar
    
    * Some style fixes in the player page
    
    * Fix bugs that happen when certain data is missing
    
    * Create function to get all tournament dates
    
    * Fix isRegistrationOpen check
    
    * Render correct calendar dates in the registration form
    
    * Create utils function for splitting calendar into week chunks + add year to the calendar date string
    
    * Render calendar correctly in the player page
    
    * Updating calendar works nicely now
    
    * Small style update
    
    * Show the current week in the calendar first
    
    * Do not reload the page after updating the calendar, use useState instead
    
    * Add markdown support to calendar
    
    * Add info on calendar supporting markdown also to the registration form
    
    * Remove body padding
    
    * Small calendar update fix
    
    * Add margin to image upload component
    
    * Update types

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export enum PlayerTitle {
+  KK = "KK",
+  MM = "MM",
+  LL = "LL",
+  TT = "TT"
+}
--- pages/api/player/create.ts
+++ pages/api/player/create.ts
@@ -1,42 +1,42 @@
+import { PlayerTitle } from "../../../lib/constants";
 import prisma from "../../../lib/prisma";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type PlayerFormData = {
-    userId: string
-    tournamentId: string
-    alias: string
-    address: string
-    learningInstitution?: string
-    eyeColor?: string
-    hair?: string
-    height?: string
-    other?: string
-    safetyNotes?: string
-    calendar?: object
-    title?: "LL" | "KK" | "MM" | "TT"
-}
+  userId: string;
+  tournamentId: string;
+  alias: string;
+  address: string;
+  learningInstitution?: string;
+  eyeColor?: string;
+  hair?: string;
+  height?: string;
+  other?: string;
+  safetyNotes?: string;
+  calendar?: object;
+  title?: PlayerTitle;
+};
 
 export default async function create(
-    req: NextApiRequest,
-    res: NextApiResponse
-  ) {
-      const playerData: PlayerFormData = JSON.parse(req.body);
-      const result = await prisma.player.create({
-        data: {
-            user: { connect: { id: playerData.userId } },
-            tournament: { connect: { id: playerData.tournamentId } },
-            alias: playerData.alias,
-            address: playerData.address,
-            learningInstitution: playerData.learningInstitution,
-            eyeColor: playerData.eyeColor,
-            hair: playerData.hair,
-            height: parseInt(playerData.height),
-            other: playerData.other,
-            safetyNotes: playerData.safetyNotes,
-            calendar: playerData.calendar,
-            title: playerData.title
-        }
-      });
-      res.status(201).send(result);
-  }
-  
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const playerData: PlayerFormData = JSON.parse(req.body);
+  const result = await prisma.player.create({
+    data: {
+      user: { connect: { id: playerData.userId } },
+      tournament: { connect: { id: playerData.tournamentId } },
+      alias: playerData.alias,
+      address: playerData.address,
+      learningInstitution: playerData.learningInstitution,
+      eyeColor: playerData.eyeColor,
+      hair: playerData.hair,
+      height: parseInt(playerData.height),
+      other: playerData.other,
+      safetyNotes: playerData.safetyNotes,
+      calendar: playerData.calendar,
+      title: playerData.title
+    }
+  });
+  res.status(201).send(result);
+}
