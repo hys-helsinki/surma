db9914e22a0d1f27e35a1ccc37736e0ab4f03f19
Erika <ersku1998@gmail.com>
2024-09-20 22:11:26 +0300

    Add active, dead and detective groups to the player table

--- components/Admin/PlayerTable.tsx
+++ components/Admin/PlayerTable.tsx
@@ -16,12 +16,17 @@ const PlayerTable = ({ userList, tournament, handleMakeWanted }) => {
 
   if (users.length === 0) return <p>Ei pelaajia</p>;
 
+  const active = users.filter((user) => user.player.state === "ACTIVE");
+  const dead = users.filter((user) => user.player.state === "DEAD");
+  const detectives = users.filter((user) => user.player.state === "DETECTIVE");
+
   return (
     <div style={{ paddingLeft: "10px" }}>
       <h3>Pelaajat</h3>
       <table>
         <tbody>
-          {users.map((user) => (
+          <tr>Elossa</tr>
+          {active.map((user) => (
             <tr key={user.id}>
               <td>
                 <Link href={`/tournaments/${tournament.id}/users/${user.id}`}>
@@ -30,47 +35,74 @@ const PlayerTable = ({ userList, tournament, handleMakeWanted }) => {
                   </a>
                 </Link>
               </td>
-              {user.player.state == "ACTIVE" ? (
-                <>
-                  <td>
-                    <button
-                      onClick={() =>
-                        handlePlayerStatusChange("DEAD", user.player.id)
-                      }
-                    >
-                      Tapa
-                    </button>
-                  </td>
-                  <td>
-                    <button onClick={() => handleMakeWanted(user.player.id)}>
-                      Etsintäkuuluta
-                    </button>
-                  </td>
-                </>
-              ) : (
-                <>
-                  <td>
-                    <button
-                      onClick={() =>
-                        handlePlayerStatusChange("ACTIVE", user.player.id)
-                      }
-                    >
-                      Herätä henkiin
-                    </button>
-                  </td>
-                  {user.player.state != "DETECTIVE" && (
-                    <td>
-                      <button
-                        onClick={() =>
-                          handlePlayerStatusChange("DETECTIVE", user.player.id)
-                        }
-                      >
-                        Etsiväksi
-                      </button>
-                    </td>
-                  )}
-                </>
-              )}
+
+              <td>
+                <button
+                  onClick={() =>
+                    handlePlayerStatusChange("DEAD", user.player.id)
+                  }
+                >
+                  Tapa
+                </button>
+              </td>
+              <td>
+                <button onClick={() => handleMakeWanted(user.player.id)}>
+                  Etsintäkuuluta
+                </button>
+              </td>
+            </tr>
+          ))}
+          <tr>Kuolleet</tr>
+          {dead.map((user) => (
+            <tr key={user.id}>
+              <td>
+                <Link href={`/tournaments/${tournament.id}/users/${user.id}`}>
+                  <a>
+                    {user.firstName} {user.lastName} ({user.player.alias})
+                  </a>
+                </Link>
+              </td>
+
+              <td>
+                <button
+                  onClick={() =>
+                    handlePlayerStatusChange("ACTIVE", user.player.id)
+                  }
+                >
+                  Herätä henkiin
+                </button>
+              </td>
+
+              <td>
+                <button
+                  onClick={() =>
+                    handlePlayerStatusChange("DETECTIVE", user.player.id)
+                  }
+                >
+                  Etsiväksi
+                </button>
+              </td>
+            </tr>
+          ))}
+          <tr>Etsivät</tr>
+          {detectives.map((user) => (
+            <tr key={user.id}>
+              <td>
+                <Link href={`/tournaments/${tournament.id}/users/${user.id}`}>
+                  <a>
+                    {user.firstName} {user.lastName} ({user.player.alias})
+                  </a>
+                </Link>
+              </td>
+              <td>
+                <button
+                  onClick={() =>
+                    handlePlayerStatusChange("ACTIVE", user.player.id)
+                  }
+                >
+                  Herätä henkiin
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
