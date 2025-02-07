2292ac1a7f3ff7f01544214d6d9604931c2751f0
Erika <ersku1998@gmail.com>
2025-02-07 21:05:36 +0200

    Add button to add more players to team

--- components/Registration/TeamForm.tsx
+++ components/Registration/TeamForm.tsx
@@ -1,4 +1,4 @@
-import { Box, Container } from "@mui/material";
+import { Box, Button, Container } from "@mui/material";
 import { Form, Formik } from "formik";
 import * as Yup from "yup";
 import Image from "next/image";
@@ -6,10 +6,16 @@ import logo from "/public/images/surma_logo.svg";
 import TextInput from "./TextInput";
 import { LoadingButton } from "@mui/lab";
 import { Tournament } from "@prisma/client";
+import { useState } from "react";
 
 const TeamForm = ({ tournament }: { tournament: Tournament }) => {
+  const [numberOfPlayers, setNumberOfPlayers] = useState(1);
+
   const submitForm = (values) => {
-    const groupedValues = Array.from([1, 2, 3, 4]).map((n) =>
+    const groupedValues = Array.from(
+      { length: numberOfPlayers },
+      (x, i) => i + 1
+    ).map((n) =>
       Object.entries(values)
         .filter((v) => v[0].includes(n.toString()))
         .map((v) => v[1])
@@ -83,39 +89,57 @@ const TeamForm = ({ tournament }: { tournament: Tournament }) => {
           })}
           onSubmit={(values) => submitForm(values)}
         >
-          <Form>
+          <Form
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                e.preventDefault();
+              }
+            }}
+          >
             <TextInput label="Joukkueen nimi" name="teamName" type="text" />
-            {Array.from([1, 2, 3, 4]).map((n) => (
-              <Box sx={{ my: 5 }} key={n}>
-                <h2>Pelaaja {n}</h2>
-                <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-                  <Box sx={{ width: "45%" }}>
-                    <TextInput
-                      label="Etunimi"
-                      name={`firstName${n}`}
-                      type="text"
-                    />
+            {Array.from({ length: numberOfPlayers }, (x, i) => i + 1).map(
+              (n) => (
+                <Box sx={{ my: 5 }} key={n}>
+                  <h2>Pelaaja {n}</h2>
+                  <Box
+                    sx={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <Box sx={{ width: "45%" }}>
+                      <TextInput
+                        label="Etunimi"
+                        name={`firstName${n}`}
+                        type="text"
+                      />
+                    </Box>
+                    <Box sx={{ width: "45%" }}>
+                      <TextInput
+                        label="Sukunimi"
+                        name={`lastName${n}`}
+                        type="text"
+                      />
+                    </Box>
                   </Box>
-                  <Box sx={{ width: "45%" }}>
-                    <TextInput
-                      label="Sukunimi"
-                      name={`lastName${n}`}
-                      type="text"
-                    />
-                  </Box>
+                  <TextInput
+                    label="Sähköpostiosoite"
+                    name={`email${n}`}
+                    type="email"
+                  />
+                  <TextInput
+                    label="Puhelinnumero"
+                    name={`phone${n}`}
+                    type="text"
+                  />
                 </Box>
-                <TextInput
-                  label="Sähköpostiosoite"
-                  name={`email${n}`}
-                  type="email"
-                />
-                <TextInput
-                  label="Puhelinnumero"
-                  name={`phone${n}`}
-                  type="text"
-                />
-              </Box>
-            ))}
+              )
+            )}
+            {numberOfPlayers < 4 && (
+              <button
+                onClick={() => setNumberOfPlayers(numberOfPlayers + 1)}
+                type="button"
+              >
+                Lisää pelaaja
+              </button>
+            )}
             <LoadingButton type="submit">Luo käyttäjät</LoadingButton>
           </Form>
         </Formik>
