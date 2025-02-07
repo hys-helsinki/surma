b61c9a35c17221a93e8aa3ac34984cddb34abeec
Erika <ersku1998@gmail.com>
2025-02-07 21:56:28 +0200

    Redirect user with session to either admin or user page from registration page

--- pages/tournaments/[tournamentId]/registration.tsx
+++ pages/tournaments/[tournamentId]/registration.tsx
@@ -3,8 +3,42 @@ import prisma from "../../../lib/prisma";
 import UserForm from "../../../components/Registration/UserForm";
 import TeamForm from "../../../components/Registration/TeamForm";
 import { Tournament } from "@prisma/client";
+import { unstable_getServerSession } from "next-auth/next";
+import { authConfig } from "../../api/auth/[...nextauth]";
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+export const getServerSideProps: GetServerSideProps = async ({
+  params,
+  req,
+  res
+}) => {
+  const session = await unstable_getServerSession(req, res, authConfig);
+
+  if (session) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: session.user.id
+      },
+      select: {
+        id: true,
+        umpire: true,
+        tournamentId: true
+      }
+    });
+    if (user.umpire) {
+      return {
+        redirect: {
+          destination: `/admin/${user.tournamentId}`,
+          permanent: false
+        }
+      };
+    }
+    return {
+      redirect: {
+        destination: `/tournaments/${user.tournamentId}/users/${user.id}`,
+        permanent: false
+      }
+    };
+  }
   let tournament = await prisma.tournament.findUnique({
     where: {
       id: params.tournamentId as string
