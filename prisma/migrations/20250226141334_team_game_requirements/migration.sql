10644f7475de67a62cc922fc4004154aacdf2358
Jesse Lankila <jesse.lankila@kodan.fi>
2025-02-26 16:13:49 +0200

    db migration

---
+++ prisma/migrations/20250226141334_team_game_requirements/migration.sql
@@ -0,0 +1,41 @@
+-- AlterTable
+ALTER TABLE "Assignment" ADD COLUMN     "teamAssignmentRingId" TEXT;
+
+-- AlterTable
+ALTER TABLE "Tournament" ADD COLUMN     "teamGame" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "teamId" TEXT;
+
+-- CreateTable
+CREATE TABLE "TeamAssignment" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "modifiedAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "teamAssignmentRingId" TEXT,
+
+    CONSTRAINT "TeamAssignment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TeamAssignmentRing" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "modifiedAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT,
+    "tournamentId" TEXT NOT NULL,
+
+    CONSTRAINT "TeamAssignmentRing_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Assignment" ADD CONSTRAINT "Assignment_teamAssignmentRingId_fkey" FOREIGN KEY ("teamAssignmentRingId") REFERENCES "TeamAssignmentRing"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TeamAssignment" ADD CONSTRAINT "TeamAssignment_teamAssignmentRingId_fkey" FOREIGN KEY ("teamAssignmentRingId") REFERENCES "TeamAssignmentRing"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TeamAssignmentRing" ADD CONSTRAINT "TeamAssignmentRing_tournamentId_fkey" FOREIGN KEY ("tournamentId") REFERENCES "Tournament"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
