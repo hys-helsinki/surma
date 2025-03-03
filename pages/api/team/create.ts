f31e3e6dcff9562a2cf5578322e2bd0d041db691
Erika <ersku1998@gmail.com>
2025-03-03 22:13:11 +0200

    Add endpoint for creating a team

---
+++ pages/api/team/create.ts
@@ -0,0 +1,32 @@
+import prisma from "../../../lib/prisma";
+import type { NextApiRequest, NextApiResponse } from "next";
+import sendEmail from "../../../lib/ses_mailer";
+
+export default async function create(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const teamData = JSON.parse(req.body);
+    const { teamName, users, tournamentId } = teamData;
+
+    console.log(teamData);
+
+    const team = await prisma.team.create({
+      data: {
+        tournament: { connect: { id: tournamentId } },
+        name: teamName,
+        users: {
+          createMany: {
+            data: users
+          }
+        }
+      },
+      include: {
+        users: true
+      }
+    });
+
+    res.json(team);
+  }
+}
