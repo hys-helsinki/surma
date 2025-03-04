f6617f1587de3cc72876b082ed557f57cc552d8b
Erika <ersku1998@gmail.com>
2025-03-04 20:56:58 +0200

    Send email to umpires when new team has been created

--- pages/api/team/create.ts
+++ pages/api/team/create.ts
@@ -10,6 +10,12 @@ export default async function create(
     const teamData = JSON.parse(req.body);
     const { teamName, users, tournamentId } = teamData;
 
+    const tournament = await prisma.tournament.findFirst({
+      where: {
+        id: tournamentId
+      }
+    });
+
     const team = await prisma.team.create({
       data: {
         tournament: { connect: { id: tournamentId } },
@@ -24,6 +30,20 @@ export default async function create(
         users: true
       }
     });
+
+    const emailContent = `Joukkueen nimi: ${
+      teamData.teamName
+    }\nPelaajat: ${team.users.map(
+      (user) =>
+        `\n${user.firstName} ${user.lastName}\n(${process.env.BASE_URL}tournaments/${tournamentId}/users/${user.id})`
+    )}`;
+
+    sendEmail(
+      "surma@salamurhaajat.net",
+      "tuomaristo@salamurhaajat.net",
+      `${tournament.name}: Uusi ilmoittautuminen (${teamData.teamName})`,
+      emailContent
+    );
 
     res.json(team);
   }
