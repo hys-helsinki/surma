1942a4d4120a831aa5f9d5130edd19250c518bc7
Erika <ersku1998@gmail.com>
2025-03-18 17:51:56 +0200

    Move admin page components to tabs

--- pages/admin/[id].tsx
+++ pages/admin/[id].tsx
@@ -1,4 +1,4 @@
-import { Grid, Box } from "@mui/material";
+import { Grid, Box, Tabs, Tab } from "@mui/material";
 import { GetServerSideProps } from "next";
 import { unstable_getServerSession } from "next-auth";
 import { useState } from "react";
@@ -8,8 +8,29 @@ import { TeamTournamentRings } from "../../components/Admin/Rings/TeamTournament
 import prisma from "../../lib/prisma";
 import { authConfig } from "../api/auth/[...nextauth]";
 import PlayerTable from "../../components/Admin/PlayerTable";
-import Link from "next/link";
-import { Field, Form, Formik } from "formik";
+import TeamTable from "../../components/Admin/TeamTable";
+import UmpireSelect from "../../components/Admin/UmpireSelect";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+const TabPanel = (props: TabPanelProps) => {
+  const { children, value, index } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+    >
+      {value === index && <Box padding={2}>{children}</Box>}
+    </div>
+  );
+};
 
 const isCurrentUserAuthorized = async (tournamentId, context) => {
   const session = await unstable_getServerSession(
@@ -47,7 +68,8 @@ export const getServerSideProps: GetServerSideProps = async ({
     },
     include: {
       player: true,
-      umpire: true
+      umpire: true,
+      team: true
     }
   });
 
@@ -57,6 +79,7 @@ export const getServerSideProps: GetServerSideProps = async ({
     },
     include: {
       user: true,
+      team: true,
       umpire: {
         include: {
           user: true
@@ -86,6 +109,13 @@ export const getServerSideProps: GetServerSideProps = async ({
   let teams = await prisma.team.findMany({
     where: {
       tournamentId: params.id as string
+    },
+    include: {
+      players: {
+        include: {
+          user: true
+        }
+      }
     }
   });
 
@@ -110,24 +140,13 @@ export default function Tournament({
   teams
 }) {
   const [rings, setRings] = useState<any[]>(ringList);
-  const [showSuccessText, setShowSuccessText] = useState(false);
+  const [value, setValue] = useState(0);
 
-  const unfinishedRegistrations = users
-    .filter((user) => !user.player && !user.umpire)
-    .sort((a, b) => a.firstName.localeCompare(b.firstName));
+  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
 
   const umpires = users.filter((user) => user.umpire);
-
-  const handleSaveUmpires = async (values) => {
-    const res = await fetch(`/api/player/umpires`, {
-      method: "PATCH",
-      body: JSON.stringify(values)
-    });
-
-    if (res.status === 200) {
-      setShowSuccessText(true);
-    }
-  };
 
   const handleMakeWanted = async (id: string) => {
     const res = await fetch(`/api/player/${id}/wanted`, {
@@ -138,96 +157,67 @@ export default function Tournament({
     setRings((prevRings) => prevRings.concat(createdRing));
   };
 
-  const formInitials = Object.assign(
-    {},
-    ...players.map((player) => ({
-      [`${player.id}`]: player.umpire ? player.umpire.id : ""
-    }))
-  );
-
   return (
     <AuthenticationRequired>
       <Box sx={{ m: 2 }}>
         <h1 style={{ width: "100%" }}>{tournament.name}</h1>
-        <Grid container>
-          <Grid item xs={12} md={6}>
-            {unfinishedRegistrations.length > 0 && (
-              <div style={{ paddingLeft: "10px", marginBottom: "30px" }}>
-                <h2>Keskeneräiset ilmoittautumiset</h2>
-                {unfinishedRegistrations.map((user) => (
-                  <div key={user.id}>
-                    <Link
-                      href={`/tournaments/${tournament.id}/users/${user.id}`}
-                    >
-                      {user.firstName} {user.lastName}
-                    </Link>
-                  </div>
-                ))}
-              </div>
-            )}
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs
+            value={value}
+            onChange={handleTabChange}
+            variant="scrollable"
+            textColor="inherit"
+            TabIndicatorProps={{
+              style: {
+                backgroundColor: "#f72a2a"
+              }
+            }}
+          >
+            <Tab label="Pelaajat" />
+            <Tab label="Ringit" />
+            <Tab label="Tuomarien asettaminen" />
+          </Tabs>
+        </Box>
+
+        <TabPanel value={value} index={0}>
+          {tournament.teamGame ? (
+            <TeamTable
+              tournament={tournament}
+              teams={teams}
+              handleMakeWanted={handleMakeWanted}
+              users={users}
+            />
+          ) : (
             <PlayerTable
               playerList={players}
               tournament={tournament}
               handleMakeWanted={handleMakeWanted}
             />
-            <h2>Pelaajien tuomarit</h2>
-            <Formik
-              enableReinitialize={true}
-              initialValues={formInitials}
-              onSubmit={(values) => {
-                handleSaveUmpires(values);
-              }}
-            >
-              <Form>
-                {players.map((player) => (
-                  <div key={player.id}>
-                    <div style={{ width: "100%" }}>
-                      <label htmlFor={`${player.id}`}>
-                        {player.user.firstName} {player.user.lastName}
-                      </label>
-                    </div>
-                    <Field
-                      name={`${player.id}`}
-                      id={`${player.id}`}
-                      as="select"
-                    >
-                      <option></option>
-                      {umpires.map((user) => (
-                        <option value={user.umpire.id} key={user.id}>
-                          {user.firstName} {user.lastName}
-                        </option>
-                      ))}
-                    </Field>
-                  </div>
-                ))}
-                <button type="submit" style={{ width: "40%" }}>
-                  Tallenna tuomarit
-                </button>
-                <p
-                  style={{ visibility: showSuccessText ? "visible" : "hidden" }}
-                >
-                  Tuomarien tallentaminen onnistui!
-                </p>
-              </Form>
-            </Formik>
-          </Grid>
-          <Grid item xs={12} md={3}>
-            {tournament.teamGame ? (
-              <TeamTournamentRings
-                tournament={tournament}
-                rings={rings}
-                teams={teams}
-                setRings={setRings}
-              />
-            ) : (
-              <TournamentRings
-                tournament={tournament}
-                players={players}
-                rings={rings}
-              />
-            )}
-          </Grid>
-        </Grid>
+          )}
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          <UmpireSelect
+            umpires={umpires}
+            players={players}
+            teamGame={tournament.teamGame}
+          />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          {tournament.teamGame ? (
+            <TeamTournamentRings
+              tournament={tournament}
+              rings={rings}
+              teams={teams}
+              setRings={setRings}
+            />
+          ) : (
+            <TournamentRings
+              tournament={tournament}
+              players={players}
+              rings={rings}
+            />
+          )}
+        </TabPanel>
       </Box>
     </AuthenticationRequired>
   );
