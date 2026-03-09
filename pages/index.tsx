378e80f053936e388753011ba6582f2fdfcf8182
Erika Ojanperä <59066641+erikao1998@users.noreply.github.com>
2026-03-09 21:22:57 +0200

    Update landing page (#157)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import { signIn } from "next-auth/react";
 import logo from "/public/images/surma_logo.svg";
-import InfoAccordion from "../components/InfoAccordion";
-import TournamentTable from "../components/TournamentTable";
+import InfoAccordion from "../components/LandingPage/InfoAccordion";
+import TournamentTable from "../components/LandingPage/TournamentTable";
 import { authConfig } from "./api/auth/[...nextauth]";
 import prisma from "../lib/prisma";
 import { unstable_getServerSession } from "next-auth/next";
@@ -64,14 +63,12 @@ export default function Home({ tournaments }) {
       className={`${styles.center} ${styles.main}`}
       style={{ padding: "20px" }}
     >
-      <h1 className={styles.container} style={{ padding: "1em" }}>
-        Surma (Murhamaster 3.0)
-      </h1>
+      <h1>Surma</h1>
       <Image
         src={logo}
         alt="logo"
-        width={200}
-        height={200}
+        width={100}
+        height={100}
         priority={true}
         style={{
           maxWidth: "100%",
@@ -79,11 +76,10 @@ export default function Home({ tournaments }) {
         }}
       />
       <h2 className={styles.container} style={{ padding: "1em" }}>
-        {" "}
-        Avoimet salamurhaturnaukset{" "}
+        Avoimet salamurhaturnaukset
       </h2>
-      <TournamentTable tournaments={tournaments}></TournamentTable>
-      <InfoAccordion></InfoAccordion>
+      <TournamentTable tournaments={tournaments} />
+      <InfoAccordion />
     </div>
   );
 }
