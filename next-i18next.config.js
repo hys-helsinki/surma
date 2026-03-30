7857304296ac777b85195bb728779639bdc98b6e
Erika Ojanperä <59066641+erikao1998@users.noreply.github.com>
2026-03-30 10:13:24 +0300

    Fix localization not working in production (#168)

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,10 @@
+const path = require("path");
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
   i18n: {
     defaultLocale: "fi",
     locales: ["fi", "en"],
     localeDetection: false
-  }
+  },
+  localePath: path.resolve("./public/locales")
 };
